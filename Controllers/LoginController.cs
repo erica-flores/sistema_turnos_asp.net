d74c50a858f65776f9db8f6f2ffe1ea693ebb9d5
erica-flores <eriflor.1991@gmail.com>
2022-01-05 20:45:34 -0300

    primer commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Turnos.Models;
+using System.Linq;
+
+
+
+namespace Turnos.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly TurnosContext _context;
+        public LoginController(TurnosContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(Login login)
+        {
+            if(ModelState.IsValid)
+            {
+                //encriptar password
+                string passwordEncriptado = Encriptar(login.Password);
+
+                var loginUsuario = _context.Login.Where(l => l.Usuario == login.Usuario && l.Password == passwordEncriptado)
+                .FirstOrDefault();
+
+                if(loginUsuario != null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }else
+                {
+                    ViewData["errorLogin"] = "Los datos ingresados son incorrectos.";
+                    return View("Index");
+                }
+            }
+            return View("Index");
+        }
+
+        public string Encriptar(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                   stringBuilder.Append(bytes[i].ToString("x2")); 
+                }
+                return stringBuilder.ToString();
+            }
+        }
+
+      //  public IActionResult Logout()
+    }
+}
---
+++ Models/Especialidad.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Turnos.Models
+{
+    public class Especialidad
+    {
+        [Key]
+        public int IdEspecialidad { get; set; }
+        [StringLength (200)]
+        [Required (ErrorMessage = "Debe ingresar una descripción")]
+        [Display (Name = "Descripción", Prompt = "Ingrese una Descripción")]
+        public string Descripcion { get; set; }
+        public List<MedicoEspecialidad> MedicoEspecialidad { get; set; }
+    }
+}
